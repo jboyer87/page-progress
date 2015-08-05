4b93b4e860caf2cb596deb7b60bdf731d5b160d6
James Boyer <aesthetics1@gmail.com>
2015-08-05 11:58:46 -0700

    v1.0
    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Page-Progress
+
+![Demo](http://www.jamesjboyer.com/projects/page-progress "Demo")
+
+**Page Progress** is a simple jQuery reading progress line. When included on a page, a progress bar will appear along the edge of the browser window to let the reader know how far they have progressed through an article.
+
+##Features
+
+**Save your place** -- As the user scrolls, progress is tracked in pixels and the URL is updated on each scroll event to include the offset from the top of the page. When a page is bookmarked, or linked to, **Page Progress** uses the value in the URL and automatically scrolls to that point.
---
+++ page-progress.js
@@ -0,0 +1,28 @@
+// Calculate the percentage the user has scrolled down the page
+// Start at 0 initially
+var scrollPercent = 0;
+var scrollPixels;
+
+// Each time the document is scrolled
+$(document).scroll(function(){
+
+	// Calculate the scroll percentage of the entire document
+		var scrollPercent = 100 * $(window).scrollTop() / ($(document).height() - $(window).height());
+
+	// Set the width of the progress bar to the current scroll percent
+	$('.ppProgressBar').width(scrollPercent+'%');
+
+	// Bookmark function
+	// Adds a hash to the url with the current position in pixels
+		var scrollPixels = $(window).scrollTop();
+		location.hash = scrollPixels;
+	// END Bookmark function
+});
+
+// Bookmark function
+$(document).ready(function(){
+	// When the document loads, sets ppBookmark to the numerical value after the hash in the url
+	var ppBookmark = window.location.hash.substring(1);
+	// Scroll to the position of ppBookmark
+	$(window).scrollTop(ppBookmark);
+});
