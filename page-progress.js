e0f3e0e3642da2d381bbe12d7e49906b4f16e443
James Boyer <aesthetics1@gmail.com>
2015-08-05 21:23:10 -0700

    doTimeout implemented
    A rate limit on the scroll function has been implemented to improve
    performance.

--- page-progress.js
+++ page-progress.js
@@ -5,18 +5,20 @@ var scrollPixels;
 
 // Each time the document is scrolled
 $(document).scroll(function(){
-
-	// Calculate the scroll percentage of the entire document
+	// doTimeout is being used to 'debounce' the scroll function to allow for better performance - thanks mdibaiee@github for the suggestion
+	$.doTimeout('scroll', 250, function(){
+		// Calculate the scroll percentage of the entire document
 		var scrollPercent = 100 * $(window).scrollTop() / ($(document).height() - $(window).height());
 
-	// Set the width of the progress bar to the current scroll percent
-	$('.ppProgressBar').width(scrollPercent+'%');
+		// Set the width of the progress bar to the current scroll percent
+		$('.ppProgressBar').width(scrollPercent+'%');
 
-	// Bookmark function
-	// Adds a hash to the url with the current position in pixels
-		var scrollPixels = $(window).scrollTop();
-		location.hash = scrollPixels;
-	// END Bookmark function
+		// Bookmark function
+		// Adds a hash to the url with the current position in pixels
+			var scrollPixels = $(window).scrollTop();
+			location.hash = scrollPixels;
+		// END Bookmark function
+	});
 });
 
 // Bookmark function
